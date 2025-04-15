167ea93870bdbb61968f1fc69ac86393716dbe96
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2025-04-15 17:51:27 -0300

    dd

--- Filas/Tp4.2_Filas/JavaApplication5/src/Tp4_2/Almacen.java
+++ Filas/Tp4.2_Filas/JavaApplication5/src/Tp4_2/Almacen.java
@@ -1,8 +1,11 @@
 
 package Tp4_2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Almacen {
     
-    
+    Queue<Productos> FilaDeEspera = new LinkedList();
     
 }
--- Filas/Tp4.2_Filas/JavaApplication5/src/Tp4_2/Productos.java
+++ Filas/Tp4.2_Filas/JavaApplication5/src/Tp4_2/Productos.java
@@ -4,6 +4,30 @@ package Tp4_2;
 
 public class Productos {
     
-    private string productos;
+    private String identificador;
+    private String tipo;
+
+    public Productos(String identificador, String tipo) {
+        this.identificador = identificador;
+        this.tipo = tipo;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
     
 }
